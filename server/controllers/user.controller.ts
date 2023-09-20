ced020298ea2c6c51f46977fc647c4fdeb72e3a4
Tanggal Abang <tanggalabang26@gmail.com>
2023-09-20 12:26:36 +0700

    register masih error api

---
+++ server/controllers/user.controller.ts
@@ -0,0 +1,84 @@
+require("dotenv").config();
+import { NextFunction, Request, Response } from "express";
+import userModel, { IUser } from "../models/user.model";
+import ErrorHandler from "../utils/ErrorHandler";
+import { CatchAsyncError } from "./../middleware/catchAsyncErrors";
+import jwt, { Secret } from "jsonwebtoken";
+import ejs from "ejs";
+import sendEmail from "../utils/sendMail";
+import path from "path";
+
+// register user
+interface IRegistrationBody {
+  name: string;
+  email: string;
+  password: string;
+  avatar?: string;
+}
+
+export const registrationUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name, email, password } = req.body;
+
+      // const isEmailExist = await userModel.findOne({ email });
+      // if (isEmailExist) {
+      //   return next(new ErrorHandler("Email already exit", 400));
+      // }
+      const user: IRegistrationBody = {
+        name,
+        email,
+        password,
+      };
+      const activationToken = createActivationToken(user);
+
+      const activationCode = activationToken.activationCode;
+
+      const data = { user: { name: user.name }, activationCode };
+
+      const html = await ejs.renderFile(
+        path.join(__dirname, "../mails/activation-mail.ejs"),
+        data
+      );
+      try {
+        await sendEmail({
+          email:user.email,
+          subject:"Activated your account",
+          template: "activation-mail.ejs",
+          data,
+        })
+
+        res.status(201).json({
+          success:true,
+          message:`Please check you email: ${user.email} to activated yout account!`,
+          activationToken: activationToken.token, 
+        })
+      } catch (error:any) {
+        return next(new ErrorHandler(error.message, 400))
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+interface IActivationToken {
+  token: String;
+  activationCode: String;
+}
+
+export const createActivationToken = (user: any): IActivationToken => {
+  const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
+
+  const token = jwt.sign(
+    {
+      user,
+      activationCode,
+    },
+    process.env.ACTIVATION_SECRET as Secret,
+    {
+      expiresIn: "5m",
+    }
+  );
+  return { token, activationCode };
+};
